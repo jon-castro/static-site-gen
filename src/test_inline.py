232f02b117ead71159e76a1da28b93d32bd0d588
jon-castro <jcastrogarcia@gmail.com>
2025-04-04 05:02:44 -0500

    Work on extracting images and links from markdown

--- src/test_inline.py
+++ src/test_inline.py
@@ -1,7 +1,13 @@
 import unittest
 
 from textnode import TextNode, TextType
-from inline import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline import (
+    split_nodes_delimiter, 
+    extract_markdown_images, 
+    extract_markdown_links, 
+    split_nodes_image, 
+    split_nodes_link
+)
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -57,3 +63,39 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         )
         self.assertListEqual([("to boot dev", "https://www.boot.dev"),
                               ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+        
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://www.google.com) and another [second link](https://www.bing.com)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://www.google.com"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second link", TextType.LINK, "https://www.bing.com"
+                ),
+            ],
+            new_nodes,
+        )
